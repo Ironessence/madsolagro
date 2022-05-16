79eb39c2c2a55e0c76d72f724b57b2122f6d69d8
Alex ianglaijsgag <alexdnc22@gmail.com>
2022-05-16 20:00:34 +0300

    added sample users in backend

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+    users: [
+        {
+            name: 'Alex',
+            email: 'admin@example.com',
+            password: bcrypt.hashSync('123456'),
+            isAdmin: true,
+        },
+        {
+            name: 'John',
+            email: 'user@example.com',
+            password: bcrypt.hashSync('123456'),
+            isAdmin: false,
+        },
+    ],
     produse: [
         {
             
@@ -44,7 +60,7 @@ const data = {
             inStoc: 10,
             reducere: '10%',
         },
-    ]
+    ],
 }
 
 export default data;
